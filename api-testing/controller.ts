dbb278469bb61e9ef4900becc0a91b8599441453
LenaTester <106830652+LenaTester@users.noreply.github.com>
2024-05-10 10:57:40 +0200

    Delete api-testing/controller.ts

--- api-testing/controller.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export class ApiControllers {
-    // https://dummyjson.com/users/1
-    async getUserById(userId: any) {
-        let r = await axios.get(`https://dummyjson.com/users/${userId}`);
-        expect(r.status).toBe(200)
-    }
-}
