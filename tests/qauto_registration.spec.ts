828843341749c2778fa153708ced8d7467f860e7
LenaTester <106830652+LenaTester@users.noreply.github.com>
2024-05-12 12:45:40 +0200

    Update qauto_registration.spec.ts

--- tests/qauto_registration.spec.ts
+++ tests/qauto_registration.spec.ts
@@ -14,7 +14,7 @@ test.describe('Tests for QAuto user registration', { tag: ["@smoke", "@regressio
     await expect(register_page.elements.registerMessage).toHaveText('Registration complete')
   })
 
-  test('User can not register, using special characters in name and lastname', async ({ page }) => {
+  test.skip('User can not register, using special characters in name and lastname', async ({ page }) => {
     const register_page = new RegistrationPage(page)
     await register_page.userRegistration(users.user2.name, users.user2.lastname, users.user2.email, 
       users.user2.password, users.user2.repeat_password)
@@ -22,14 +22,14 @@ test.describe('Tests for QAuto user registration', { tag: ["@smoke", "@regressio
     await expect(register_page.elements.invalidFeedbackMessage.nth(1)).toHaveText('Last name is invalid')
   })
 
-  test('User can not register, using invalid email format', async ({ page }) => {
+  test.skip('User can not register, using invalid email format', async ({ page }) => {
     const register_page = new RegistrationPage(page)
     await register_page.userRegistration(users.user3.name, users.user3.lastname, users.user3.email, 
       users.user3.password, users.user3.repeat_password)
     await expect(register_page.elements.invalidFeedbackMessage.nth(0)).toHaveText('Email is incorrect')
   })
 
-  test('User can not register with weak password', async ({ page }) => {
+  test.skip('User can not register with weak password', async ({ page }) => {
     const register_page = new RegistrationPage(page)
     await register_page.userRegistration(users.user4.name, users.user4.lastname, users.user4.email, 
       users.user4.password, users.user4.repeat_password)
@@ -41,14 +41,14 @@ one integer, one capital, and one small letter')
 one integer, one capital, and one small letter')
   })
 
-  test('User can not register with non-matching passwords', async ({ page }) => {
+  test.skip('User can not register with non-matching passwords', async ({ page }) => {
     const register_page = new RegistrationPage(page)
     await register_page.userRegistration(users.user5.name, users.user5.lastname, users.user5.email, 
       users.user5.password, users.user5.repeat_password)
     await expect(register_page.elements.invalidFeedbackMessage.nth(0)).toHaveText('Passwords do not match')
   })
 
-  test('User can not register with empty creds', async ({ page }) => {
+  test.skip('User can not register with empty creds', async ({ page }) => {
     const register_page = new RegistrationPage(page)
     await register_page.userRegistration(users.user6.name, users.user6.lastname, users.user6.email, 
       users.user6.password, users.user6.repeat_password)
