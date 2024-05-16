9a5926a30f7d742adfb9fd969ea4045050aeb54e
LenaTester <106830652+LenaTester@users.noreply.github.com>
2024-05-16 12:00:31 +0200

    Update auth.setup.spec.ts

--- tests/auth.setup.spec.ts
+++ tests/auth.setup.spec.ts
@@ -2,7 +2,7 @@ import { test as setup, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { users } from '../user_data';
 
-const authFile = 'playwright/.auth/user.json';
+const authFile = '../playwright/.auth/user.json';
 
 setup('authenticate', async ({ page }) => {
   const loginPage = new LoginPage(page)
